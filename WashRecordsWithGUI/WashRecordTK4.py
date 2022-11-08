1bc3aeef586fc28337d2d96a9d950fc3dc8f69d3
raphavcruz <114435190+raphavcruz@users.noreply.github.com>
2022-11-08 10:08:06 -0500

    Nov 8th, 2022

--- WashRecordsWithGUI/WashRecordTK4.py
+++ WashRecordsWithGUI/WashRecordTK4.py
@@ -1,13 +1,14 @@
 import tkinter as tk
-from tkinter import CENTER, END, Widget, ttk
+from tkinter import END, ttk
 from tkinter.font import BOLD
 from PIL import ImageTk
+from tkcalendar import *
 
 bg_color = "#000000"
 
 def enter_data():
 
-    DateEntry = date_entry.get()
+    Cal_Entry = date_entry_cal.get()
     NameEntry = name_entry.get()
     WashEntry = wash_spinbox.get()
     ProdEntry = prod_combobox.get()
@@ -17,7 +18,7 @@ def enter_data():
     ReportEntry = report_entry.get(1.0, END)
 
     print(
-    "Date:", DateEntry,", "
+    "Date:", Cal_Entry,", "
     "Initials:", NameEntry,", "
     "Wash Count:", WashEntry,", "
     "Product:", ProdEntry,", "
@@ -30,10 +31,15 @@ def enter_data():
 #Initialize app
 gui = tk.Tk()
 gui.title("Wash Record Generator - Sinclair Interplanetary by Rocket Lab")
-#gui.eval("tk::PlaceWindow . center")
-gui.configure(bg=bg_color)
-gui.geometry("600x850")
 
+#App placement on screen
+#gui.eval("tk::PlaceWindow . left")
+x = gui.winfo_screenwidth() // 2
+y = int(gui.winfo_screenheight() * 0.1)
+gui.geometry("600x850+" + str(x) + "+" + str(y))
+gui.configure(bg=bg_color)
+
+#Frames inside parent window (gui)
 frame0 = tk.Frame(gui, width=600, height= 107, bg=bg_color, padx=0, pady=0)
 frame0.pack()
 frame0.pack_propagate(False)
@@ -50,10 +56,11 @@ frame3 = tk.Frame(gui, width=600, height= 150, bg=bg_color, padx=0, pady=0)
 frame3.pack()
 frame3.pack_propagate(False)
 
+#paddying for all widgets inside frame1
 for widget in frame1.winfo_children():
     widget.grid_configure(padx=5, pady=8)
 
-#user_input
+#user_input, labels & entries
 date_label = tk.Label(frame1, text="Date:", bg=bg_color, fg="white", font=("TkMenuFont", 11), anchor="e", width=19)
 date_label.grid(row=0, column=0, padx=10, pady=1)
 name_label = tk.Label(frame1, text="Initials:", bg=bg_color, fg="white", font=("TkMenuFont", 11), anchor="e", width=19)
@@ -71,8 +78,8 @@ serial_label.grid(row=6, column=0, padx=10, pady=1)
 report_label = tk.Label(frame1, text="Paste wash report here:", bg=bg_color, fg="white", font=("TkMenuFont", 11), anchor="e", width=19)
 report_label.grid(row=7, column=0, padx=10, pady=1)
 
-date_entry = tk.Entry(frame1, width=60)
-date_entry.grid(row=0, column=1)
+date_entry_cal = DateEntry(frame1, width=57)
+date_entry_cal.grid(row=0, column=1)
 name_entry = tk.Entry(frame1, width=60)
 name_entry.grid(row=1, column=1)
 wash_spinbox = tk.Spinbox(frame1, width=58, values=["001", "002", "003", "004", "005", "006", "007", "008", "009"])
@@ -88,6 +95,7 @@ serial_entry.grid(row=6, column=1)
 report_entry = tk.Text(frame1, width=45, height=25)
 report_entry.grid(row=7, column=1, padx=10, pady=2)
 
+#Button and banners
 button = tk.Button(frame2, text="GENERATE REPORT", font=("TkHeadingFont", 12, BOLD), bg="#D72020", fg="#FFFFFF", cursor="hand2", activebackground="#DC143C", activeforeground="#FFFFFF", command=enter_data)
 button.grid(row=15, column=0, sticky="news", padx=10, pady=10)
 
@@ -101,5 +109,4 @@ logo_widget = tk.Label(frame3, image=logo_img, bg=bg_color)
 logo_widget.image = logo_img
 logo_widget.pack()
 
-# run app
 gui.mainloop()
